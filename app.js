d7c4cefe61a3016f3353f19cc3a365ac5c46ecae
Birk <birk.kensen@gmail.com>
2021-10-15 09:17:43 +0200

    made the cards load from JS and some styling

--- app.js
+++ app.js
@@ -1,16 +1,158 @@
 //let emojis = [ '🐒', '🐒', '🦊', '🦊', '🐯', '🐯', '🐕', '🐕', '🐷', '🐷', '🐄', '🐄', '🐪', '🐪', '🐘', '🐘', '🦏', '🦏', '🦒', '🦒', '🐹', '🐹', '🐨', '🐨' ]
 
-const cards = document.querySelectorAll(".flip-card-inner");
+let animalTheme = [
+  {
+    content: '🐒',
+    id: 'a'
+  },
+  {
+    content: '🦊',
+    id: 'b'
+  },
+  {
+    content: '🐯',
+    id: 'c'
+  },
+  {
+    content: '🐕',
+    id: 'd'
+  },
+  {
+    content: '🐷',
+    id: 'e'
+  },
+  {
+    content: '🐄',
+    id: 'f'
+  },
+  {
+    content: '🐪',
+    id: 'g'
+  },
+  {
+    content: '🐘',
+    id: 'h'
+  },
+  {
+    content: '🦒',
+    id: 'i'
+  },
+  {
+    content: '🐹',
+    id: 'j'
+  },
+  {
+    content: '🐨',
+    id: 'k'
+  },
+  {
+    content: '🐝',
+    id: 'l'
+  },
+];
+let halloweenTheme = [
+  {
+    content: '🎃',
+    id: 'a',
+  },
+  {
+    content: '🕯',
+    id: 'b',
+  },
+  {
+    content: '👻',
+    id: 'c',
+  },
+  {
+    content: '🕸',
+    id: 'd',
+  },
+  {
+    content: '🕷',
+    id: 'e',
+  },
+  {
+    content: '💀',
+    id: 'f',
+  },
+  {
+    content: '⚰️',
+    id: 'g',
+  },
+  {
+    content: '🦇',
+    id: 'h',
+  },
+  {
+    content: '😈',
+    id: 'i',
+  },
+  {
+    content: '🧟‍♂️',
+    id: 'j',
+  },
+  {
+    content: '🌕',
+    id: 'k',
+  },
+  {
+    content: '🧛',
+    id: 'l',
+  },
+  ];
 
+//Duplicate array and merge it with the original
+function duplicateArray(array) {
+  let copy = array;
+  array = array.concat(copy);
+  return array;
+}
+
+let cardArray = duplicateArray(animalTheme);
 let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
+
+function createCards () {
+  for(i = 0; i < cardArray.length; i++) {
+    card(cardArray[i])
+  }
+  const cards = document.querySelectorAll(".flip-card-inner");
+  cards.forEach(card => {
+    card.addEventListener("click", flipCard)
+  })
+}
+
+function card(card) {
+  document.querySelector(".flip-card-container").innerHTML += 
+  `
+<div class="flip-card-inner" data-letter="${card.id}">
+    <div class="flip-card-front">
+  </div>
+  <div class="flip-card-back">
+      <p>${card.content}</p>
+  </div>
+</div>
+  `
+}
+
+function startGame() { 
+  document.querySelector(".flip-card-container").innerHTML = `
+  <h1 class="card-flip-title">Memory Game</h1>
+  <div class="card-score-container">
+    <h3>Time</h3>
+    <h3>Flips: 0</h3>
+  </div>
+  `; 
+  createCards();
+  shuffleCards();
+}
 
 function flipCard() {
   if (lockBoard) return;
   if (this === firstCard) return;
   this.classList.add("flip");
-
+  countMatchedCards();
   if (!hasFlippedCard) {
     hasFlippedCard = true;
     firstCard = this;
@@ -19,8 +161,8 @@ function flipCard() {
 
   secondCard = this;
   hasFlippedCard = false;
-
   checkForMatch();
+  
 }
 
 function checkForMatch() {
@@ -51,11 +193,22 @@ function resetBoard() {  //ES6 destructuring assignment
   [firstCard, secondCard] = [null, null];
 }
 
-(function shuffle() {   //IIFE
+function shuffleCards() {   //IIFE
+  const cards = document.querySelectorAll(".flip-card-inner");
   cards.forEach((card) => {
     let ramdomPos = Math.floor(Math.random() * 24);
     card.style.order = ramdomPos;
   });
-})();
+};
 
-cards.forEach((card) => card.addEventListener("click", flipCard));
+function countMatchedCards() {
+  const isFlipped = document.querySelectorAll(".flip");
+  if(isFlipped.length === cardArray.length) {
+    setTimeout(() => {
+      startGame()
+    }, 1000)
+    
+  }
+}
+// cards.forEach((card) => card.addEventListener("click", flipCard));
+
