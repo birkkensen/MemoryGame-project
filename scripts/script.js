537ebd0a84d33790dd71b7983f3cc9f23454c0b9
Birk <birk.kensen@gmail.com>
2021-10-20 11:13:14 +0200

    fixed close button on modals

--- scripts/script.js
+++ scripts/script.js
@@ -1,42 +1,34 @@
-
-
-
 //burgermenu
 const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
-    
-    burger.addEventListener('click', function () {
-            nav.classList.toggle('nav-active');
-       
-       
-            navLinks.forEach((link, index) => {
-               if (link.style.animation) {
-                   link.style.animation = ''
-                  
-               }
-               else{
-                   link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s`;
-                console.log(index / 7); 
-            }
-       
-        });
-            //burger animation
-            burger.classList.toggle('toggle');
-   
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+
+  burger.addEventListener("click", function () {
+    nav.classList.toggle("nav-active");
+
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 1.5
+        }s`;
+        console.log(index / 7);
+      }
     });
-}
+    //burger animation
+    burger.classList.toggle("toggle");
+  });
+};
 
 navSlide();
 
-  
 //popup
 // Get the modal
 var modal = document.getElementById("myModal");
 
 var rulesmodal = document.getElementById("rules");
-
 
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
@@ -44,31 +36,35 @@ var btn = document.getElementById("myBtn");
 var rulesbtn = document.getElementById("myBtnrules");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+// var span = document.getElementsByClassName("close")[0];
+var span = document.querySelectorAll(".close");
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
+// When the user clicks the button, open the modal
+btn.onclick = function () {
   modal.style.display = "block";
-}
+};
 
-rulesbtn.onclick = function() {
-    rulesmodal.style.display = "block";
-}
+rulesbtn.onclick = function () {
+  rulesmodal.style.display = "block";
+};
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+// span.onclick = function () {
+//   modal.style.display = "none";
+// };
+span.forEach((span) => {
+  span.addEventListener("click", () => {
+    modal.style.display = "none";
+    rulesmodal.style.display = "none";
+  });
+});
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-}
-
-
-
+};
 
 // LocalStorage
 
@@ -77,22 +73,20 @@ const inpValue = document.getElementById("inpValue");
 const btnInsert = document.getElementById("btnInsert");
 const lsOutput = document.getElementById("lsOutput");
 
-btnInsert.onclick = function (){
-    const key = inpKey.value;
-    const value = inpValue.value;
+btnInsert.onclick = function () {
+  const key = inpKey.value;
+  const value = inpValue.value;
 
-    console.log (key);
-    console.log (value);
+  console.log(key);
+  console.log(value);
 
-   if (key && value){
-        localStorage.setItem(key, value);
-        
-    }
+  if (key && value) {
+    localStorage.setItem(key, value);
+  }
 };
 
-for (let i = 0; i <localStorage.length; i++){
-    const key = localStorage.key(i);
-    const value = localStorage.getItem(key);
-    lsOutput.innerHTML += `${key}: ${value}<br />`;
+for (let i = 0; i < localStorage.length; i++) {
+  const key = localStorage.key(i);
+  const value = localStorage.getItem(key);
+  lsOutput.innerHTML += `${key}: ${value}<br />`;
 }
-
