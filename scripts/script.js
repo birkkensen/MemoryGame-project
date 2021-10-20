e6eb9a9721f8edf8eaafe6fc18f24b0355f6f3f3
ChrisCepeda <chriscepeda@gmail.com>
2021-10-20 11:21:27 +0200

    changes

--- scripts/script.js
+++ scripts/script.js
@@ -60,12 +60,20 @@ span.onclick = function() {
   modal.style.display = "none";
 }
 
+
+  
+
+
+
+
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
 }
+
+
 
 
 
@@ -81,8 +89,6 @@ btnInsert.onclick = function (){
     const key = inpKey.value;
     const value = inpValue.value;
 
-    console.log (key);
-    console.log (value);
 
    if (key && value){
         localStorage.setItem(key, value);
