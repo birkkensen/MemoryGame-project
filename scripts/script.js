b3bb2431e76a2b928baaeff6fb51fa3239c957f8
ChrisCepeda <chriscepeda@gmail.com>
2021-10-20 13:22:18 +0200

    more changes

--- scripts/script.js
+++ scripts/script.js
@@ -30,10 +30,14 @@ var modal = document.getElementById("myModal");
 
 var rulesmodal = document.getElementById("rules");
 
+
+
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
 
 var rulesbtn = document.getElementById("myBtnrules");
+
+
 
 // Get the <span> element that closes the modal
 // var span = document.getElementsByClassName("close")[0];
@@ -48,6 +52,7 @@ rulesbtn.onclick = function () {
   rulesmodal.style.display = "block";
 };
 
+
 // When the user clicks on <span> (x), close the modal
 // span.onclick = function () {
 //   modal.style.display = "none";
@@ -57,6 +62,7 @@ span.forEach((span) => {
   span.addEventListener("click", () => {
     modal.style.display = "none";
     rulesmodal.style.display = "none";
+   
   });
 });
 
